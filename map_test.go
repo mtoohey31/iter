3d2fd96d7ed47535e4cb029495d535c13f6484cc
Matthew Toohey <contact@mtoohey.com>
2022-01-07 22:15:49 -0500

    test: add TestMapSame

--- map_test.go
+++ map_test.go
@@ -2,8 +2,24 @@ package iter
 
 import (
 	"reflect"
+	"strings"
 	"testing"
 )
+
+func TestMapSame(t *testing.T) {
+	expected := []string{"ITEM1", "ITEM2"}
+	var iter *Iter[string] = FromSlice([]string{"item1", "item2"}).MapSame(func(s string) string { return strings.ToUpper(s) })
+
+	actual := iter.Collect()
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("got %v, expected %v", actual, expected)
+	}
+
+	if len(iter.Collect()) != 0 {
+		t.Fatalf("original iterator was not drained")
+	}
+}
 
 func TestMap(t *testing.T) {
 	expected := []int{5, 5}
