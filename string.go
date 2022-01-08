d73144397a13d4335f4bf57b4a42af408e89aab1
Matthew Toohey <contact@mtoohey.com>
2022-01-08 11:47:30 -0500

    feat: add SplitByString

--- string.go
+++ string.go
@@ -39,3 +39,33 @@ func (i *splitByRuneInner) Next() (string, error) {
 		return i.string[i.index : i.index+runeIndex], nil
 	}
 }
+
+type splitByStringInner struct {
+	string string
+	sep    string
+	index  int
+}
+
+func SplitByString(s string, sep string) *Iter[string] {
+	return WithInner[string](&splitByStringInner{s, sep, 0})
+}
+
+func (i *splitByStringInner) HasNext() bool {
+	return i.index < len(i.string)
+}
+
+func (i *splitByStringInner) Next() (string, error) {
+	if !i.HasNext() {
+		return "", IteratorExhaustedError
+	}
+
+	sepIndex := strings.Index(i.string[i.index:], i.sep)
+
+	if sepIndex == -1 {
+		defer func() { i.index = len(i.string) }()
+		return i.string[i.index:], nil
+	} else {
+		defer func() { i.index = i.index + sepIndex + len(i.sep) }()
+		return i.string[i.index : i.index+sepIndex], nil
+	}
+}
--- string_test.go
+++ string_test.go
@@ -26,3 +26,14 @@ func TestSplitByRune(t *testing.T) {
 		t.Fatalf("got %v, expected %v", actual, expected)
 	}
 }
+
+func TestSplitByString(t *testing.T) {
+	iter := SplitByString("the quick brown fox jumped over the lazy dogs", "the ")
+
+	actual := iter.Collect()
+	expected := []string{"", "quick brown fox jumped over ", "lazy dogs"}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("got %v, expected %v", actual, expected)
+	}
+}
