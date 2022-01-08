6e3d4bdaf71f756d987bb8b70814094396919df4
Matthew Toohey <contact@mtoohey.com>
2022-01-08 00:40:50 -0500

    test: add TestCollect

--- iter_test.go
+++ iter_test.go
@@ -1,1 +1,17 @@
 package iter
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCollect(t *testing.T) {
+	expected := []string{"item1", "item2"}
+	var iter *Iter[string] = FromSlice(expected)
+
+	actual := iter.Collect()
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("got %v, expected %v", actual, expected)
+	}
+}
