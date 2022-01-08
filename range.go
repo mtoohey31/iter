05a1797504c200347116d723434435781e113e92
Matthew Toohey <contact@mtoohey.com>
2022-01-08 17:32:08 -0500

    refactor!: vendor constraints package while it won't import properly

--- range.go
+++ range.go
@@ -1,16 +1,12 @@
 package iter
 
-type rangeInt interface {
-	int | int8 | int16 | int32 | int64
-}
-
-type rangeInner[T rangeInt] struct {
+type rangeInner[T integer] struct {
 	curr T
 	end  T
 	step T
 }
 
-func Range[T rangeInt](start T, end T, step T) *Iter[T] {
+func Range[T integer](start T, end T, step T) *Iter[T] {
 	return WithInner[T](&rangeInner[T]{curr: start, end: end, step: step})
 }
 
