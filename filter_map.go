3aa6731f05dff2d7a2e65ca577f2cd2f033e761b
Matthew Toohey <contact@mtoohey.com>
2022-01-07 22:16:00 -0500

    feat: add FilterMap and FilterMapSame

---
+++ filter_map.go
@@ -0,0 +1,50 @@
+package iter
+
+type filterMapInner[T any, U any] struct {
+	inner         *Iter[T]
+	filterMapFunc func(T) (U, error)
+	cachedNext    *U
+	zero          U // TODO: find a less gross way to produce a zero value of a generic type
+}
+
+func (i *Iter[T]) FilterMapSame(f func(T) (T, error)) *Iter[T] {
+	return WithInner[T](filterMapInner[T, T]{inner: i, filterMapFunc: f})
+}
+
+func FilterMap[T any, U any](i *Iter[T], f func(T) (U, error)) *Iter[U] {
+	return WithInner[U](filterMapInner[T, U]{inner: i, filterMapFunc: f})
+}
+
+func (i filterMapInner[T, U]) findNext() (U, error) {
+	for {
+		next, err := i.inner.Next()
+
+		if err != nil {
+			break
+		}
+
+		if mappedNext, err := i.filterMapFunc(next); err == nil {
+			return mappedNext, nil
+		}
+	}
+	return i.zero, IteratorExhaustedError
+}
+
+func (i filterMapInner[T, U]) HasNext() bool {
+	if i.cachedNext != nil {
+		return true
+	}
+
+	next, err := i.findNext()
+	i.cachedNext = &next
+	return err == nil
+}
+
+func (i filterMapInner[T, U]) Next() (U, error) {
+	if i.cachedNext != nil {
+		defer func() { i.cachedNext = nil }()
+		return *i.cachedNext, nil
+	} else {
+		return i.findNext()
+	}
+}
---
+++ filter_map_test.go
@@ -0,0 +1,38 @@
+package iter
+
+import (
+	"errors"
+	"reflect"
+	"strconv"
+	"testing"
+)
+
+func TestFilterMapSame(t *testing.T) {
+	var iter *Iter[int] = FromSlice([]int{1, 2, 3, 4}).FilterMapSame(func(i int) (int, error) {
+		if i%2 != 0 {
+			return 0, errors.New("")
+		} else {
+			return i * 2, nil
+		}
+	})
+
+	actual := iter.Collect()
+	expected := []int{4, 8}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("got %v, expected %v", actual, expected)
+	}
+}
+
+func TestFilterMap(t *testing.T) {
+	var iter *Iter[int] = FilterMap(FromSlice([]string{"1", "nope", "2", "un-uh"}), func(s string) (int, error) {
+		return strconv.Atoi(s)
+	})
+
+	actual := iter.Collect()
+	expected := []int{1, 2}
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Fatalf("got %v, expected %v", actual, expected)
+	}
+}
