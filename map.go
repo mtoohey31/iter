72b33c6697ce24066352ab912c3df16fd101f69d
Matthew Toohey <contact@mtoohey.com>
2022-01-08 18:34:21 -0500

    feat: add KVZip for iterating over mappings

--- map.go
+++ map.go
@@ -1,23 +1,54 @@
 package iter
 
-type mapInner[T, U any] struct {
+import "github.com/barweiss/go-tuple"
+
+type mapDataInner[T comparable, U any] struct {
+	innerKeys *Iter[T]
+	mapping   map[T]U
+}
+
+func KVZip[T comparable, U any](m map[T]U) *Iter[tuple.T2[T, U]] {
+	var keys []T
+
+	for key := range m {
+		keys = append(keys, key)
+	}
+
+	return WithInner[tuple.T2[T, U]](&mapDataInner[T, U]{innerKeys: Elems(keys), mapping: m})
+}
+
+func (i *mapDataInner[T, U]) HasNext() bool {
+	return i.innerKeys.HasNext()
+}
+
+func (i *mapDataInner[T, U]) Next() (tuple.T2[T, U], error) {
+	key, err := i.innerKeys.Next()
+
+	if err == nil {
+		return tuple.New2(key, i.mapping[key]), nil
+	} else {
+		return tuple.T2[T, U]{}, IteratorExhaustedError
+	}
+}
+
+type mapFuncInner[T, U any] struct {
 	inner   *Iter[T]
 	mapFunc func(T) U
 }
 
 func (i *Iter[T]) MapSame(f func(T) T) *Iter[T] {
-	return WithInner[T](&mapInner[T, T]{inner: i, mapFunc: f})
+	return WithInner[T](&mapFuncInner[T, T]{inner: i, mapFunc: f})
 }
 
 func Map[T, U any](i *Iter[T], f func(T) U) *Iter[U] {
-	return WithInner[U](&mapInner[T, U]{inner: i, mapFunc: f})
+	return WithInner[U](&mapFuncInner[T, U]{inner: i, mapFunc: f})
 }
 
-func (i *mapInner[T, U]) HasNext() bool {
+func (i *mapFuncInner[T, U]) HasNext() bool {
 	return i.inner.HasNext()
 }
 
-func (i *mapInner[T, U]) Next() (U, error) {
+func (i *mapFuncInner[T, U]) Next() (U, error) {
 	next, err := i.inner.Next()
 
 	if err == nil {
--- map_test.go
+++ map_test.go
@@ -4,17 +4,34 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/barweiss/go-tuple"
 	"mtoohey.com/iter/test"
 )
 
-func TestMapSame(t *testing.T) {
+func TestMapData(t *testing.T) {
+	expected := []tuple.T2[string, int]{
+		tuple.New2("1", 1),
+		tuple.New2("2", 2),
+		tuple.New2("3", 3),
+		tuple.New2("4", 4),
+	}
+
+	m := make(map[string]int)
+	for _, v := range expected {
+		m[v.V1] = v.V2
+	}
+
+	test.AssertDeepEq(KVZip(m).Collect(), expected, t)
+}
+
+func TestMapSameFunc(t *testing.T) {
 	iter := Elems([]string{"item1", "item2"}).MapSame(func(s string) string { return strings.ToUpper(s) })
 
 	test.AssertDeepEq(iter.Collect(), []string{"ITEM1", "ITEM2"}, t)
 	test.Assert(!iter.HasNext(), t)
 }
 
-func TestMap(t *testing.T) {
+func TestMapFunc(t *testing.T) {
 	iter := Map(Elems([]string{"item1", "item2"}), func(s string) int { return len(s) })
 
 	test.AssertDeepEq(iter.Collect(), []int{5, 5}, t)
