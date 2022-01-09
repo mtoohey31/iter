0f1291b0f6ceea5f243e2bbb98719ab5196f86e5
Matthew Toohey <contact@mtoohey.com>
2022-01-08 19:58:43 -0500

    test: simplify TestChain

--- chain_test.go
+++ chain_test.go
@@ -7,11 +7,6 @@ import (
 )
 
 func TestChain(t *testing.T) {
-	iter1 := Elems([]int{1, 2})
-	iter2 := Elems([]int{3, 4})
-
-	actual := iter1.Chain(iter2).Collect()
-	expected := []int{1, 2, 3, 4}
-
-	test.AssertDeepEq(actual, expected, t)
+	actual := Elems([]int{1, 2}).Chain(Elems([]int{3, 4})).Collect()
+	test.AssertDeepEq(actual, []int{1, 2, 3, 4}, t)
 }
