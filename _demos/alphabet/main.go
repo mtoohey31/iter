152b43c2a698bb2f8a46b38354710aaca51b2615
Matthew Toohey <contact@mtoohey.com>
2022-03-16 11:19:18 -0400

    feat: v1 release

---
+++ _demos/alphabet/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "mtoohey.com/iter"
+
+func IterAlphabet() iter.Iter[rune] {
+	c := 'a'
+	return func() (rune, bool) {
+		if c <= 'z' {
+			res := c
+			c++
+			return res, true
+		} else {
+			return rune(0), false
+		}
+	}
+}
+
+func main() {
+	println(string(IterAlphabet().Collect())) // produces: abcdefghijklmnopqrstuvwxyz
+	cycle, _ := IterAlphabet().Take(3).Cycle()
+	cycledRunes := make([]rune, 21)
+	cycle.CollectInto(cycledRunes)
+	println(string(cycledRunes)) // produces: abcabcabcabcabcabcabc
+}
