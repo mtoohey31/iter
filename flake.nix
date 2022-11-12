7066b3127487a0772a2fd703956ac975bf18d773
Matthew Toohey <contact@mtoohey.com>
2022-11-11 21:23:42 -0500

    build: update flake, remove yaegi, add benchstat

--- flake.nix
+++ flake.nix
@@ -8,43 +8,38 @@
       url = "github:mitranim/gow";
       flake = false;
     };
-    yaegi-src = {
-      url = "github:traefik/yaegi";
+    perf-src = {
+      url = "git+https://go.googlesource.com/perf";
       flake = false;
     };
   };
 
-  outputs = { self, nixpkgs, utils, gow-src, yaegi-src }:
+  outputs = { self, nixpkgs, utils, gow-src, perf-src }:
     utils.lib.eachDefaultSystem (system: with import nixpkgs
       {
         overlays = [
-          (final: prev: {
-            gow = final.buildGoModule rec {
+          (final: _: {
+            benchstat = final.buildGoModule {
+              pname = "benchstat";
+              version = perf-src.shortRev;
+              src = perf-src;
+              subPackages = [ "cmd/benchstat" ];
+              vendorSha256 = "ZIkH3LBzrvqWEN6m4fpU2cmOXup9LLU3FiFooJJtiOk=";
+            };
+            gow = final.buildGoModule {
               pname = "gow";
-              version = "0.1.0";
+              version = gow-src.shortRev;
               src = gow-src;
               vendorSha256 = "o6KltbjmAN2w9LMeS9oozB0qz9tSMYmdDW3CwUNChzA=";
             };
-            yaegi-rlwrapped = final.buildGoModule rec {
-              pname = "yaegi-rlwrapped";
-              version = yaegi-src.shortRev;
-              src = yaegi-src;
-              vendorSha256 = "pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
-              GOROOT = "${final.go_1_18}/share/go";
-              fixupPhase = ''
-                mv $out/bin/yaegi $out/bin/.yaegi-wrapped
-                printf "#!${final.bash}/bin/bash\n${final.rlwrap}/bin/rlwrap $out/bin/.yaegi-wrapped \"\$@\"\n" > $out/bin/yaegi
-                chmod +x $out/bin/yaegi
-              '';
-            };
           })
         ]; inherit system;
-      }; rec {
+      }; {
       devShells = rec {
-        ci = mkShell { packages = [ go mdsh revive ]; };
+        ci = mkShell { packages = [ benchstat go mdsh revive ]; };
 
         default = mkShell {
-          packages = ci.nativeBuildInputs ++ [ gopls gow yaegi-rlwrapped ];
+          packages = ci.nativeBuildInputs ++ [ gopls gow ];
         };
       };
     });
